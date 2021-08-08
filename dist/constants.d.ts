ed70641725be42c5334e78c86ae5290df18b9091
senior-vova <vladimir.reg.acc@gmail.com>
2021-08-08 20:13:17 +0400

    Init

--- dist/constants.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const NMA_MODULE_CONFIGS = "NMA_MODULE_CONFIGS";
-export declare const JWT_MODULE_CONFIGS = "JWT_MODULE_CONFIGS";
--- dist/jwt-module/jwt.module.d.ts
+++
@@ -1,5 +0,0 @@
-import { DynamicModule } from "@nestjs/common";
-import { JWTModuleSetupOptions } from "src/interfaces";
-export declare class JWTModule {
-    static forRoot(options: JWTModuleSetupOptions): DynamicModule;
-}
