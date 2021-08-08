0c96c984df5376ae1bacfde7372bbbbedba4867e
senior-vova <vladimir.reg.acc@gmail.com>
2021-08-08 20:55:23 +0400

    Blank

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { Route, Switch } from "react-router";
-import { MainAppHOC } from "./hoc/MainAppHOC";
+import { HashRouter } from "react-router-dom";
 import HomePage from "./pages/home";
 
 const App: React.FC = () => {
   return (
-    <Switch>
-      <Route path="/" render={() => <HomePage />} />
-    </Switch>
+    <HashRouter>
+      <Switch>
+        <Route path="/" render={() => <HomePage />} />
+      </Switch>
+    </HashRouter>
   );
 };
 
-export default MainAppHOC(App);
+export default App;
